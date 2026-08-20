21a0146ab414f5dc528881dea865a4d0dce9ab93
TaTo30 <aldo.hernandez3012@gmail.com>
2026-08-20 12:31:46 -0600

    add signature help in cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,7 @@ return {
       "hrsh7th/cmp-path",
       'hrsh7th/cmp-cmdline',
       "hrsh7th/cmp-vsnip",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     config = function()
       local cmp = require("cmp")
@@ -43,6 +44,7 @@ return {
           { name = "nvim_lsp" },
           { name = "vsnip" },
           { name = "path" },
+          { name = "nvim_lsp_signature_help" },
         }, {
           { name = "buffer" },
         }),
