d8d2332f8f0dc801e564b75dc56da13e3e4c1e06
TaTo30 <aldo.hernandez3012@gmail.com>
2026-08-25 10:42:59 -0600

    cmp snipped fixed

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,7 @@ return {
       "hrsh7th/cmp-path",
       'hrsh7th/cmp-cmdline',
       "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
       "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     config = function()
